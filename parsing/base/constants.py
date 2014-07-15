2b79f1e0d80728e1518bfc9a3226025c0d958c47
kid143 <primecriminalkid@gmail.com>
2014-07-15 23:11:56 +0800

    create base components

---
+++ parsing/base/constants.py
@@ -0,0 +1,30 @@
+__author__ = 'kid143'
+
+
+"""Constants"""
+
+
+TOKEN_NAMES = [
+    'n/a',
+    'COMMA',
+    'LSQRTBRACKET',
+    'RSQRTBRACKET',
+    'LRNDBRACKET',
+    'RRNDBRACKET',
+    'LBRBRACKET',
+    'RBRBRACKET',
+    'COLON',
+    'DOT',
+    'SEP',
+]
+
+COMMA = 1
+LSQRTBRACKET = 2
+RSQRTBRACKET = 3
+LRNDBRACKET = 4
+RRNDBRACKET = 5
+LBRBRACKET = 6
+RBRBRACKET = 7
+COLON = 8
+DOT = 9
+SEP = 10
---
+++ parsing/base/lexer.py
@@ -0,0 +1,64 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+__author__ = 'kid143'
+
+
+"""This is base components
+"""
+
+from .constants import TOKEN_NAMES
+
+
+class ParseError(Exception):
+    pass
+
+
+class Token:
+    """
+    Token object
+    """
+    def __init__(self, token_type, string):
+        self.token_type = token_type
+        self.string = string
+
+    def __str__(self):
+        return "<Token type: {type}, string: '{string}'>".format(type=TOKEN_NAMES[self.token_type],
+                                                                 string=self.string)
+
+
+class Lexer:
+    """
+    Lexer base class
+    """
+    def __init__(self, input_string):
+        self.input_string = input_string
+        self.p = 0
+        self.c = self.input_string[self.p]
+
+    def consume(self):
+        self.advance()
+        self.ws()
+
+    def advance(self):
+        self.p += 1
+        try:
+            self.c = self.input_string[self.p]
+        except IndexError:
+            self.c = None
+
+    def match(self, x):
+        """This method is used to ensure x is the next character in the input stream.
+           Currently it is not called.
+        """
+        if self.c == x:
+            self.consume()
+        else:
+            raise ParseError('Expect {expect}; found:{found}'.format(expect=x, found=self.c))
+
+    def next_token(self):
+        raise NotImplementedError
+
+    def ws(self):
+        raise NotImplementedError
